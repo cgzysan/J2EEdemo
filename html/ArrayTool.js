4fddd74643ff6012904807f4a5ff91b4cb727595
cgzysan <cgzysan@icloud.com>
2018-12-25 17:41:16 +0800

    Dec25 PM

--- html/ArrayTool.js
+++ html/ArrayTool.js
@@ -20,7 +20,7 @@ function ArrayTool() {
     }
 
     // 找元素的索引值
-    this.searchEle(arr, target) {
+    this.searchEle = function(arr, target) {
         for (var i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
                 return i;
@@ -29,10 +29,10 @@ function ArrayTool() {
         return -1;
     }
 
-    // 冒泡排序
-    this.bubbleSort(arr) {
-        for (var i = 0; i < arr.length; i++) {
-            for (var j = i + 1; j < arr.length - i; j++) {
+    // 选择排序
+    this.selectSort = function(arr) {
+        for (var i = 0; i < arr.length - 1; i++) {
+            for (var j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     var temp = arr[i];
                     arr[i] = arr[j];
@@ -41,5 +41,17 @@ function ArrayTool() {
             }
         }
     }
-    https: //www.cnblogs.com/sincoolvip/p/5449202.html
+
+    // 冒泡排序
+    this.bubbleSort = function(arr) {
+        for (var i = 0; i < arr.length - 1; i++) {
+            for (var j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    var temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
