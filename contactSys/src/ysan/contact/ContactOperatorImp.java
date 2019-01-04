bc6ca2230b759583dc550f1191e5c04f68e4f7f2
cgzysan <cgzysan@icloud.com>
2019-01-04 17:39:22 +0800

    Jan 4 PM

--- contactSys/src/ysan/contact/ContactOperatorImp.java
+++ contactSys/src/ysan/contact/ContactOperatorImp.java
@@ -1,27 +1,107 @@
 package ysan.contact;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
 
 public class ContactOperatorImp implements ContactOperator{
 
 	@Override
 	public void addContact(Contact contact) {
-		
+		try {
+			File file = new File("~/Users/ysan/Documents/contact.xml");
+			Document doc = null;
+			Element rootElem = null;
+			if (!file.exists()) {
+				doc = DocumentHelper.createDocument();
+				doc.getRootElement();
+			} else {
+				doc = XMLUtil.getDocument();
+				rootElem = doc.getRootElement();
+			}
+			
+			Element contactElem = rootElem.addElement("contact");
+			contactElem.addAttribute("id", contact.getId());
+			contactElem.addElement("name").setText(contact.getName());
+			contactElem.addElement("gender").setText(contact.getGender());
+			contactElem.addElement("age").setText(contact.getAge()+"");
+			contactElem.addElement("phone").setText(contact.getPhone());
+			contactElem.addElement("email").setText(contact.getEmail());
+			contactElem.addElement("qq").setText(contact.getQq());
+			
+			XMLUtil.write2xml(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
 	}
 
 	@Override
 	public void updateContact(Contact contact) {
-		
+		try {
+			Document doc = XMLUtil.getDocument();
+			Element contactElem = (Element) doc.selectSingleNode("//contact[@id='" +contact.getId() +"']");
+			
+			contactElem.element("name").setText(contact.getName());
+			contactElem.element("gender").setText(contact.getGender());
+			contactElem.element("age").setText(contact.getAge()+"");
+			contactElem.element("phone").setText(contact.getPhone());
+			contactElem.element("email").setText(contact.getEmail());
+			contactElem.element("qq").setText(contact.getQq());
+			
+			XMLUtil.write2xml(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
 	}
 
 	@Override
 	public void deleteContact(String id) {
-		
+		try {
+			//1.读取xml文件
+			Document doc = XMLUtil.getDocument();
+			//2.查询需要删除id的contact
+			Element contactElem = (Element)doc.selectSingleNode("//contact[@id='"+id+"']");
+			//删除标签
+			contactElem.detach();
+			
+			//3.把Document写出到xml文件
+			XMLUtil.write2xml(doc);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
 	public List<Contact> findAll() {
-		return null;
+		//1.读取xml文件
+		Document doc = XMLUtil.getDocument();
+		
+		//2.创建List对象
+		List<Contact> list = new ArrayList<Contact>();
+		//3.读取contact标签
+		List<Element> conList = (List<Element>)doc.selectNodes("//contact");
+		for(Element e:conList){
+			//创建COntact对象
+			Contact c = new Contact();
+			c.setId(e.attributeValue("id"));
+			c.setName(e.elementText("name"));
+			c.setGender(e.elementText("gender"));
+			c.setAge(Integer.parseInt(e.elementText("age")));
+			c.setPhone(e.elementText("phone"));
+			c.setEmail(e.elementText("email"));
+			c.setQq(e.elementText("qq"));
+			//把Contact放入list中
+			list.add(c);
+		}
+		return list;
 	}
 
 }
---
+++ day10/src/ysan/test/TestSevlet.java
@@ -0,0 +1,44 @@
+package ysan.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestSevlet
+ */
+@WebServlet("/TestSevlet")
+public class TestSevlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestSevlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setCharacterEncoding("utf-8");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().write("我是谁");
+		System.out.println("doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
